100d612bd2b1071bec2052c4b117810b1de0d85c
V Harshith <v.harshith@ad.infosys.com>
2018-03-26 11:43:14 +0530

    ofc commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const mongojs = require("mongojs");
+const express = require("express");
+const path = require('path');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+// To sign up into MongoDB:
+var databaseUrl = "flatStore"; 
+var collections = ["gems"]
+var db = mongojs(databaseUrl, collections);
+
+var app = express();
+app.use(express.static(__dirname));
+app.engine('html', ejs.renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+	res.render('./flatlanders.html');
+});
+
+app.get('/retrieve',function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+	db.gems.find(function(err, testData) {
+		res.send(testData);
+	});	
+});
+
+app.post('/addReviews',function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.setHeader('Access-Control-Allow-Origin', '*');	
+	db.gems.update({'name': req.body[0]},{$push :{'reviews':req.body[1]}},function(err,doc){
+		console.log("Successfully inserted");
+	});
+	db.gems.update({ },{ $pull: { "reviews": { "stars": { "$exists": false } } } },{ "multi": true },function(err,doc){
+		//console.log("DEL");
+	});
+	res.json(req.body[1]);
+});
+
+var server = app.listen(3000,()=>{
+	console.log("Server is running at port 3000..");
+});
